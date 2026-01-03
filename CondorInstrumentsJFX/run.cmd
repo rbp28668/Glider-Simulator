5c757bb632af39e09a407923dae69ec4b58529c8
rbp28668 <rbp28668@gmail.com>
2026-01-03 20:54:56 +0000

    updated

--- CondorInstrumentsJFX/run.cmd
+++ CondorInstrumentsJFX/run.cmd
@@ -1,1 +1,4 @@
-%JAVA_HOME%\bin\java -jar CIJFX.jar --panel=combined.xml --dialog=true --diagnostics=true
+REM %JAVA_HOME%\bin\java -jar CIJFX.jar --panel=combined.xml --dialog=true --diagnostics=true
+
+"%JAVA_HOME%\bin\java" -jar CIJFX.jar --panel=combined.xml use=1
+
